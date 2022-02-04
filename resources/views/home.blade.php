020edc9d11318d094359719cb8061aebf9fcd42f
mubtasimrobin <mubtasimrobin@gmail.com>
2022-02-04 20:47:56 +0600

    Added Access Control

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,6 +18,29 @@
                     
                     <a href="/posts/create" class="btn btn-light">Create Post</a>
                     <h3>Your Blog Posts</h3>
+                    @if(count($posts) > 0)
+                        <table class="table table-striped">
+                            <tr>
+                                <th>Title</th>
+                                <th></th>
+                                <th></th>
+                            </tr> 
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td>{{$post->title}}</td>
+                                    <td><a href="/posts/{{$post->id}}/edit" class="btn btn-light">Edit</a></td>
+                                    <td>
+                                        {!!Form::open(['action' => ['PostsController@destroy', $post->id], 'method' => 'POST', 'class' => 'float-right'])!!}
+                                        {{Form::hidden('_method', 'DELETE')}}
+                                        {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                        {!!Form::close()!!}
+                                    </td>
+                                </tr>
+                            @endforeach  
+                        </table>
+                    @else
+                        <p>You have no post</p>
+                    @endif
                 </div>
             </div>
         </div>
